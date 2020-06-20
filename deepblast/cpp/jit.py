9c2f94654f9532d6f26e9744fdeb50b68d18a73a
Jamie Morton <jamietmorton@gmail.com>
2020-06-20 14:32:39 -0400

    removing cpp files from this branch

--- deepblast/cpp/jit.py
+++
@@ -1,3 +0,0 @@
-from torch.utils.cpp_extension import load
-nw_cpp = load(name="nw_cpp", sources=["nw.cpp"], verbose=True)
-help(nw_cpp)
--- deepblast/cpp/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, Extension
-from torch.utils import cpp_extension
-
-
-setup(name='nw_cpp',
-      ext_modules=[cpp_extension.CppExtension(
-          'nw_cpp', ['nw.cpp'])],
-      cmdclass={'build_ext': cpp_extension.BuildExtension})
