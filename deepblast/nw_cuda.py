e83595d7c40642a395f14ad46cd8a171bb971f6d
Robert Blackwell <rblackwell@flatironinstitute.org>
2020-06-25 11:21:58 -0400

    Add NW cuda _backward_pass implementation

--- deepblast/nw_cuda.py
+++ deepblast/nw_cuda.py
@@ -24,17 +24,17 @@ def _soft_max(X, A):
 def _forward_pass(theta, A, Q):
     maxargs = numba.cuda.local.array(3, numba.float64)
     V = numba.cuda.local.array((2, 2048), numba.float64)
+    N, M = theta.shape
 
-    last = 0
-    curr = 1
-    for j in range(theta.shape[0] + 1):
+    last, curr = 0, 1
+    for j in range(M + 1):
         V[last, j] = 0.0
-    V[1][0] = 0.0
+    V[curr][0] = 0.0
 
     Q[-1, -1] = 1.0
 
-    for i in range(1, theta.shape[0] + 1):
-        for j in range(1, theta.shape[1] + 1):
+    for i in range(1, N + 1):
+        for j in range(1, M + 1):
             maxargs[0] = A + V[last, j]  # x
             maxargs[1] = V[last, j-1]    # m
             maxargs[2] = A + V[curr, j-1]  # y
@@ -45,9 +45,35 @@ def _forward_pass(theta, A, Q):
         last = curr
         curr = 1 - curr
 
+    return V[last, M]
+
 
 @cuda.jit
-def _forward_pass_batch(theta, A, Q):
+def _forward_pass_batch(theta, A, Q, Vt):
     batchid = cuda.grid(1)
     if batchid < theta.shape[0]:
-        _forward_pass(theta[batchid], A[0], Q[batchid])
+        Vt[batchid] = _forward_pass(theta[batchid], A[0], Q[batchid])
+
+
+@cuda.jit(device=True)
+def _backward_pass(Et, Q, E):
+    m, x, y = 1, 0, 2
+    n_1, m_1, _ = Q.shape
+    N, M = n_1 - 2, m_1 - 2
+
+    E[N + 1, M + 1] = Et
+    Q[N + 1, M + 1] = 1
+    for ir in range(1, N + 1):
+        i = N + 1 - ir
+        for jr in range(1, M + 1):
+            j = M + 1 - jr
+            E[i, j] = Q[i + 1, j, x] * E[i + 1, j] + \
+                Q[i + 1, j + 1, m] * E[i + 1, j + 1] + \
+                Q[i, j + 1, y] * E[i, j + 1]
+
+
+@cuda.jit
+def _backward_pass_batch(Et, Q, E):
+    batchid = cuda.grid(1)
+    if batchid < E.shape[0]:
+        _backward_pass(Et[0], Q[batchid], E[batchid])
--- deepblast/tests/test_nw_cuda.py
+++ deepblast/tests/test_nw_cuda.py
@@ -1,5 +1,5 @@
-from deepblast.nw_cuda import _forward_pass_batch
-from deepblast.nw import _forward_pass_numba
+from deepblast.nw_cuda import _forward_pass_batch, _backward_pass_batch
+from deepblast.nw import _forward_pass_numba, _backward_pass_numba
 import numpy as np
 import time
 from numba import cuda
@@ -9,39 +9,46 @@ N, M = (800, 800)
 np.random.seed(1)
 theta = np.random.uniform(size=(nbatch, N, M)).astype(np.float32)
 
-Q = np.zeros((nbatch, N+2, M+2, 3), dtype=np.float32)
+Q = np.empty((nbatch, N+2, M+2, 3), dtype=np.float32)
 d_Q = cuda.device_array_like(Q)
 
 A = np.ones(1)
 d_A = cuda.to_device(A)
+
+Vt = np.empty((nbatch), dtype=np.float32)
+d_Vt = cuda.device_array_like(Vt)
+
+E = np.empty((nbatch, N + 2, M + 2))
+d_E = cuda.device_array_like(E)
+
+Et = np.ones(1)
+d_Et = cuda.to_device(Et)
+
 
 threadsperblock = 32
 blockspergrid = (nbatch + (threadsperblock - 1)) // threadsperblock
 
 start = time.time()
 d_theta = cuda.to_device(theta)
-_forward_pass_batch[blockspergrid, threadsperblock](d_theta, d_A, d_Q)
+_forward_pass_batch[blockspergrid, threadsperblock](d_theta, d_A, d_Q, d_Vt)
+_backward_pass_batch[blockspergrid, threadsperblock](d_Et, d_Q, d_E)
 d_Q.copy_to_host(Q)
+d_Vt.copy_to_host(Vt)
+d_E.copy_to_host(E)
 print("time: ", time.time() - start)
 
 start = time.time()
 d_theta = cuda.to_device(theta)
-_forward_pass_batch[blockspergrid, threadsperblock](d_theta, d_A, d_Q)
+_forward_pass_batch[blockspergrid, threadsperblock](d_theta, d_A, d_Q, d_Vt)
 d_Q.copy_to_host(Q)
+d_Vt.copy_to_host(Vt)
+d_E.copy_to_host(E)
 print("time: ", time.time() - start)
 
 
-_forward_pass_numba(theta[0], A[0])
-numba_Q = []
-start = time.time()
-for i in range(10):
-    numba_Q.append(_forward_pass_numba(theta[i], A[0])[1])
+Vt_numba, Q_numba = _forward_pass_numba(theta[0], A[0])
+E_numba = _backward_pass_numba(Et[0], Q[0])
+print(np.linalg.norm(Q_numba - Q[0]) / np.sqrt(Q[0].size))
+print(np.linalg.norm(E_numba - E[0]) / np.sqrt(E[0].size))
 
-print("time: ", time.time() - start)
-
-norms = []
-for i in range(10):
-    norms.append(np.linalg.norm(Q[i] - numba_Q[i]))
-
-print(np.mean(np.array(norms)) / np.sqrt(N*M*3))
-
+print(Vt[0] - Vt_numba)
