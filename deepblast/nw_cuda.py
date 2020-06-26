afee1e29072988977c3f83a7a8555f98fcf9b011
Robert Blackwell <rblackwell@flatironinstitute.org>
2020-06-26 10:11:24 -0400

    Port untested adjoint calls to CUDA

--- deepblast/nw_cuda.py
+++ deepblast/nw_cuda.py
@@ -2,6 +2,9 @@ import numba
 from numba import cuda
 from math import log, exp
 
+max_cols = 2048
+float_type = numba.float32
+
 
 @cuda.jit(device=True)
 def _soft_max(X, A):
@@ -21,9 +24,22 @@ def _soft_max(X, A):
 
 
 @cuda.jit(device=True)
+def _soft_max_hessian_product(P, Z, res):
+    prod = numba.cuda.local.array(3, float_type)
+
+    total = 0.0
+    for i in range(3):
+        prod[i] = P[i] * Z[i]
+        total += prod[i]
+
+    for i in range(3):
+        res[i] = prod[i] - P[i] * total
+
+
+@cuda.jit(device=True)
 def _forward_pass(theta, A, Q):
-    maxargs = numba.cuda.local.array(3, numba.float64)
-    V = numba.cuda.local.array((2, 2048), numba.float64)
+    maxargs = numba.cuda.local.array(3, float_type)
+    V = numba.cuda.local.array((2, max_cols), float_type)
     N, M = theta.shape
 
     last, curr = 0, 1
@@ -36,8 +52,8 @@ def _forward_pass(theta, A, Q):
     for i in range(1, N + 1):
         for j in range(1, M + 1):
             maxargs[0] = A + V[last, j]  # x
-            maxargs[1] = V[last, j-1]    # m
-            maxargs[2] = A + V[curr, j-1]  # y
+            maxargs[1] = V[last, j - 1]  # m
+            maxargs[2] = A + V[curr, j - 1]  # y
 
             v = _soft_max(maxargs, Q[i, j])
             V[curr, j] = theta[i - 1, j - 1] + v
@@ -75,5 +91,70 @@ def _backward_pass(Et, Q, E):
 @cuda.jit
 def _backward_pass_batch(Et, Q, E):
     batchid = cuda.grid(1)
-    if batchid < E.shape[0]:
+    if batchid < Q.shape[0]:
         _backward_pass(Et[0], Q[batchid], E[batchid])
+
+
+@cuda.jit(device=True)
+def _adjoint_forward_pass(Q, Ztheta, ZA, Qd, V):
+    Vd = numba.cuda.local.array((2, max_cols), float_type)
+    maxargs = numba.cuda.local.array(3, float_type)
+    N, M = Ztheta.shape
+    N, M = N - 2, M - 2
+    # Qd = np.zeros((N + 2, M + 2, 3))   # N x M x S
+
+    last, curr = 0, 1
+    for j in range(M + 1):
+        Vd[last, j] = 0.0
+    Vd[curr][0] = 0.0
+
+    for i in range(1, N + 1):
+        for j in range(1, M + 1):
+            maxargs[0] = ZA + Vd[last, j]  # x
+            maxargs[1] = ZA + Vd[last, j - 1]  # m
+            maxargs[2] = ZA + Vd[curr, j - 1]  # y
+            Vd[curr, j] = Ztheta[i, j] + \
+                Q[i, j, 0] * maxargs[0] + \
+                Q[i, j, 1] * maxargs[1] + \
+                Q[i, j, 2] * maxargs[2]
+            Qd[i, j] = _soft_max_hessian_product(Q[i, j], maxargs, Qd[i, j])
+
+        last = curr
+        curr = 1 - curr
+
+    return Vd[last, M]
+
+
+@cuda.jit
+def _adjoint_forward_pass_batch(Q, Ztheta, ZA, Vd, Qd):
+    batchid = cuda.grid(1)
+    if batchid < Q.shape[0]:
+        Vd[batchid] = _adjoint_forward_pass(Q[batchid], Ztheta[batchid],
+                                            ZA[batchid], Vd[batchid],
+                                            Qd[batchid])
+
+
+@cuda.jit(device=True)
+def _adjoint_backward_pass(E, Q, Qd, Ed):
+    m, x, y = 1, 0, 2
+    n_1, m_1, _ = Q.shape
+    N, M = n_1 - 2, m_1 - 2
+    # Ed = np.zeros((N + 2, M + 2))
+    for ir in range(1, N + 1):
+        i = N + 1 - ir
+        for jr in range(1, M + 1):
+            j = M + 1 - jr
+            Ed[i, j] = Qd[i + 1, j, x] * E[i + 1, j] + \
+                Q[i + 1, j, x] * Ed[i + 1, j] + \
+                Qd[i + 1, j + 1, m] * E[i + 1, j + 1] + \
+                Q[i + 1, j + 1, m] * Ed[i + 1, j + 1] + \
+                Qd[i, j + 1, y] * E[i, j + 1] + \
+                Q[i, j + 1, y] * Ed[i, j + 1]
+
+
+@cuda.jit
+def _adjoint_backward_pass_batch(E, Q, Qd, Ed):
+    batchid = cuda.grid(1)
+    if batchid < Q.shape[0]:
+        _adjoint_backward_pass(E[batchid], Q[batchid], Qd[batchid],
+                               Ed[batchid])
