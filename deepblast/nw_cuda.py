04ad98531e4b4e4bd90809ebad8f72f8a84a7710
Robert Blackwell <rblackwell@flatironinstitute.org>
2020-07-01 08:50:16 -0400

    Begin implementing CUDA routines into autograd classes

--- deepblast/nw_cuda.py
+++ deepblast/nw_cuda.py
@@ -1,13 +1,16 @@
 import numba
+import torch
+import torch.nn as nn
 from numba import cuda
 from math import log, exp
 
 max_cols = 2048
 float_type = numba.float32
+tpb = 32  # threads per block
 
 
 @cuda.jit(device=True)
-def _soft_max(X, A):
+def _soft_max_device(X, A):
     M = max(X[0], X[1], X[2])
 
     S = 0.0
@@ -37,7 +40,7 @@ def _soft_max_hessian_product(P, Z, res):
 
 
 @cuda.jit(device=True)
-def _forward_pass(theta, A, Q):
+def _forward_pass_device(theta, A, Q):
     maxargs = numba.cuda.local.array(3, float_type)
     V = numba.cuda.local.array((2, max_cols), float_type)
     N, M = theta.shape
@@ -55,7 +58,7 @@ def _forward_pass(theta, A, Q):
             maxargs[1] = V[last, j - 1]  # m
             maxargs[2] = A + V[curr, j - 1]  # y
 
-            v = _soft_max(maxargs, Q[i, j])
+            v = _soft_max_device(maxargs, Q[i, j])
             V[curr, j] = theta[i - 1, j - 1] + v
 
         last = curr
@@ -65,10 +68,10 @@ def _forward_pass(theta, A, Q):
 
 
 @cuda.jit
-def _forward_pass_batch(theta, A, Q, Vt):
+def _forward_pass_kernel(theta, A, Q, Vt):
     batchid = cuda.grid(1)
     if batchid < theta.shape[0]:
-        Vt[batchid] = _forward_pass(theta[batchid], A[0], Q[batchid])
+        Vt[batchid] = _forward_pass_device(theta[batchid], A[0], Q[batchid])
 
 
 @cuda.jit(device=True)
@@ -89,14 +92,14 @@ def _backward_pass(Et, Q, E):
 
 
 @cuda.jit
-def _backward_pass_batch(Et, Q, E):
+def _backward_pass_kernel(Et, Q, E):
     batchid = cuda.grid(1)
     if batchid < Q.shape[0]:
         _backward_pass(Et[0], Q[batchid], E[batchid])
 
 
 @cuda.jit(device=True)
-def _adjoint_forward_pass(Q, Ztheta, ZA, Qd):
+def _adjoint_forward_pass_device(Q, Ztheta, ZA, Qd):
     Vd = numba.cuda.local.array((2, max_cols), float_type)
     maxargs = numba.cuda.local.array(3, float_type)
     N, M = Ztheta.shape
@@ -125,11 +128,11 @@ def _adjoint_forward_pass(Q, Ztheta, ZA, Qd):
 
 
 @cuda.jit
-def _adjoint_forward_pass_batch(Q, Ztheta, ZA, Vd, Qd):
+def _adjoint_forward_pass_kernel(Q, Ztheta, ZA, Vd, Qd):
     batchid = cuda.grid(1)
     if batchid < Q.shape[0]:
-        Vd[batchid] = _adjoint_forward_pass(Q[batchid], Ztheta[batchid],
-                                            ZA[0], Qd[batchid])
+        Vd[batchid] = _adjoint_forward_pass_device(Q[batchid], Ztheta[batchid],
+                                                   ZA[0], Qd[batchid])
 
 
 @cuda.jit(device=True)
@@ -151,8 +154,163 @@ def _adjoint_backward_pass(E, Q, Qd, Ed):
 
 
 @cuda.jit
-def _adjoint_backward_pass_batch(E, Q, Qd, Ed):
+def _adjoint_backward_pass_kernel(E, Q, Qd, Ed):
     batchid = cuda.grid(1)
     if batchid < Q.shape[0]:
         _adjoint_backward_pass(E[batchid], Q[batchid], Qd[batchid],
                                Ed[batchid])
+
+
+class NeedlemanWunschFunction(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, theta, A, operator):
+        if operator != 'softmax':
+            raise NotImplementedError(
+                "CUDA variant only supports 'softmax' operator")
+        if theta.dtype != torch.float32:
+            raise TypeError("CUDA variant only supports torch.float32 type")
+
+        # Return both the alignment matrix
+        B, N, M = theta.shape
+
+        Q = torch.empty((B, N + 2, M + 2, 3), dtype=theta.dtype)
+        Vt = torch.empty((B), dtype=theta.dtype)
+        bpg = (B + (tpb - 1)) // tpb  # blocks per grid
+
+        d_theta = cuda.to_device(theta.detach().numpy())
+        d_A = cuda.to_device(A.detach().numpy())
+        d_Q = cuda.device_array_like(Q.detach().numpy())
+        d_Vt = cuda.device_array_like(Vt.detach().numpy())
+
+        _forward_pass_kernel[tpb, bpg](d_theta, d_A, d_Q, d_Vt)
+        d_Q.copy_to_host(Q.detach().numpy())
+        d_Vt.copy_to_host(Vt.detach().numpy())
+
+        ctx.save_for_backward(theta, A, Q)
+        ctx.others = operator
+        ctx.device_arrays = d_theta, d_A, d_Q, d_Vt
+        return Vt
+
+    @staticmethod
+    def backward(ctx, Et):
+        """
+        Parameters
+        ----------
+        ctx : ?
+           Some autograd context object
+        Et : torch.Tensor
+           Last alignment trace (scalar value)
+        """
+        theta, A, Q = ctx.saved_tensors
+        operator = ctx.others
+        print(ctx.device_arrays[0])
+        E, A = NeedlemanWunschFunctionBackward.apply(theta, A, Et, Q, operator)
+        return E[:, 1:-1, 1:-1], A, None, None, None
+
+
+class NeedlemanWunschFunctionBackward(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, theta, A, Et, Q, operator):
+        if operator != 'softmax':
+            raise NotImplementedError(
+                "CUDA variant only supports 'softmax' operator")
+
+        B, N, M = theta.shape
+        E = torch.empty((B, N + 2, M + 2), dtype=theta.dtype)
+
+        d_Et = cuda.to_device(Et.detach().numpy)
+        d_Q = cuda.to_device(Q.detach().numpy())
+        d_E = cuda.device_array_like(E.detach.numpy())
+
+        _backward_pass_kernel(d_Et, d_Q, d_E)
+        d_E.copy_to_host(E.detach().numpy())
+        # almost certainly not necessary. only assigning Q[:, -1, -1] = 1
+        d_Q.copy_to_host(Q.detach().numpy())
+
+        ctx.save_for_backward(E, Q)
+        ctx.others = operator
+        return E, A
+
+    @staticmethod
+    def backward(ctx, Ztheta, ZA):
+        """
+        Parameters
+        ----------
+        ctx : ?
+           Some autograd context object
+        Ztheta : torch.Tensor
+            Derivative of theta of dimension N x M
+        ZA : torch.Tensor
+            Derivative of transition matrix of dimension 3 x 3
+        """
+        E, Q = ctx.saved_tensors
+        # operator = ctx.others
+
+        B, N, M = Ztheta.shape
+
+        Qd = torch.empty((B, N + 2, M + 2, 3), dtype=Ztheta.dtype)
+        Vtd = torch.empty(B, dtype=Ztheta.dtype)
+        Ed = torch.empty((B, N + 2, M + 2), dtype=Ztheta.dtype)
+
+        d_Q = cuda.to_device(Q.detach().numpy())
+        d_Ztheta = cuda.to_device(Ztheta.detach().numpy())
+        d_ZA = cuda.to_device(ZA.detach().numpy())
+        d_Vtd = cuda.device_array_like(Vtd.detach().numpy())
+        d_Qd = cuda.device_array_like(Qd.detach().numpy())
+
+        d_E = cuda.to_device(E.detach().numpy())
+        d_Ed = cuda.device_array_like(Ed.detach().numpy())
+
+        _adjoint_forward_pass_kernel(d_Q, d_Ztheta, d_ZA, d_Vtd, d_Qd)
+        _adjoint_backward_pass_kernel(d_E, d_Q, d_Qd, d_Ed)
+
+        d_Vtd.to_host(Vtd.detach().numpy())
+        d_Ed.to_host(Ed.detach().numpy())
+
+        Ed = Ed[:, 1:-1, 1:-1]
+        return Ed, None, Vtd, None, None, None
+
+
+class NeedlemanWunschDecoder(nn.Module):
+    def __init__(self, operator):
+        super().__init__()
+        self.operator = operator
+
+    def forward(self, theta, A):
+        return NeedlemanWunschFunction.apply(theta, A, self.operator)
+
+    def traceback(self, grad):
+        """ Computes traceback
+
+        Parameters
+        ----------
+        grad : torch.Tensor
+            Gradients of the alignment matrix of dimension N x M.
+
+        Returns
+        -------
+        states : list of tuple
+            Indices representing matches.
+        """
+        # m, x, y = 1, 0, 2
+        N, M = grad.shape
+        states = torch.zeros(max(N, M))
+        T = max(N, M)
+        i, j = N - 1, M - 1
+        states = [(i, j)]
+        for t in reversed(range(T)):
+            idx = torch.Tensor([[i - 1, j], [i - 1, j - 1], [i, j - 1]]).long()
+            ij = torch.argmax(
+                torch.Tensor(
+                    [grad[i - 1, j], grad[i - 1, j - 1], grad[i, j - 1]]))
+            i, j = int(idx[ij][0]), int(idx[ij][1])
+            states.append((i, j))
+        return states[::-1]
+
+    def decode(self, theta, A):
+        """ Shortcut for doing inference. """
+        with torch.enable_grad():
+            nll = self.forward(theta, A)
+            v = torch.sum(nll)
+            v_grad, _ = torch.autograd.grad(v, (theta, A), create_graph=True)
+        return v_grad
