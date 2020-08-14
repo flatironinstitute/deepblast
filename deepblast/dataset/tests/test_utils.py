fixing the gap-masking. however, its a bit slow

deepblast/dataset/tests/test_utils.py
@@ -279,7 +279,7 @@ class TestPreprocess(unittest.TestCase):
     def test_gap_mask(self):
         s = ":11::22:"
         N, M = 6, 6
-        res = gap_mask(s, N, M)
+        res = gap_mask(s)
         exp = np.array(
             [
                 [1, 0, 0, 0, 0, 0],
@@ -294,7 +294,7 @@ class TestPreprocess(unittest.TestCase):
 
         s = ":11:.:22:"
         N, M = 7, 7
-        res = gap_mask(s, N, M)
+        res = gap_mask(s)
         exp = np.array(
             [
                 [1, 0, 0, 0, 0, 0, 0],
@@ -307,6 +307,19 @@ class TestPreprocess(unittest.TestCase):
             ]
         )
         npt.assert_equal(res, exp)
+
+    def test_gap_mask2(self):
+        s = (
+            '222222222222222222.11112222222222222222222222222'
+            '222222222222222222222222222222222222222222222222'
+            '22222222...::::::..:2:22::2:::::::..11.111...::.'
+            '::::::::::.::::......:::::::::::222:.::::::::.11'
+            '.:::::::::.:22.::::::::::::2:::::::::::::::1::..'
+            '.::::::::::::::::::::::22:2:2::::::::::1::::::::'
+            '::::22222::::::::::1::::::.'
+        )
+        # N, M = 197, 283
+        res = gap_mask(s)
 
 
     def test_replace_orphans_small(self):

deepblast/dataset/utils.py
@@ -27,7 +27,7 @@ def tmstate_f(z):
         return m
 
 
-def clip_boundaries(X, Y, A):
+def clip_boundaries(X, Y, A, st):
     """ Remove xs and ys from ends. """
     if A[0] == m:
         first = 0
@@ -42,7 +42,8 @@ def clip_boundaries(X, Y, A):
     X_ = X[first:last].replace('-', '')
     Y_ = Y[first:last].replace('-', '')
     A_ = A[first:last]
-    return X_, Y_, A_
+    st_ = st[first:last]
+    return X_, Y_, A_, st_
 
 
 def state_diff_f(X):
@@ -238,16 +239,20 @@ def collate_f(batch):
     states = [x[2] for x in batch]
     alignments = [x[3] for x in batch]
     paths = [x[4] for x in batch]
+    masks = [x[5] for x in batch]
     max_x = max(map(len, genes))
     max_y = max(map(len, others))
     B = len(genes)
     dm = torch.zeros((B, max_x, max_y))
     p = torch.zeros((B, max_x, max_y))
+    G = torch.zeros((B, max_x, max_y)).bool()
+    G.requires_grad = False
     for b in range(B):
         n, m = len(genes[b]), len(others[b])
         dm[b, :n, :m] = alignments[b]
         p[b, :n, :m] = paths[b]
-    return genes, others, states, dm, p
+        G[b, :n, :m] = masks[b].bool()
+    return genes, others, states, dm, p, G
 
 
 def path_distance_matrix(pi):
@@ -277,40 +282,73 @@ def path_distance_matrix(pi):
     return Pdist
 
 # Preprocessing functions
-def gap_mask(states: np.array, N : int, M : int):
-    """ Builds a mask for all gaps (0s are gaps, 1s are matches)
+# def gap_mask(states: np.array):
+#     """ Builds a mask for all gaps (0s are gaps, 1s are matches)
+#
+#     Parameters
+#     ----------
+#     states : np.array
+#        List of alignment states
+#
+#     Returns
+#     -------
+#     mask : np.array
+#        Masked array.
+#
+#     Notes
+#     -----
+#     Gaps and mismatches (denoted by `.`) are all masked here.
+#     """x
+#     i, j = 0, 0
+#     res = []
+#     coords = []
+#     data = []
+#     for k in range(len(states)):
+#         # print(i, j, k, states[k])
+#         if states[k] == '1':
+#             coords.append((i, j))
+#             data.append(0)
+#             i += 1
+#         elif states[k] == '2':
+#             coords.append((i, j))
+#             data.append(0)
+#             j += 1
+#         elif states[k] == ':':
+#             coords.append((i, j))
+#             data.append(1)
+#             i += 1
+#             j += 1
+#         elif states[k] == '.':
+#             coords.append((i, j))
+#             data.append(0)
+#             i += 1
+#             j += 1
+#         else:
+#             raise ValueError(f'{states[k]} is not recognized')
+#     rows, cols = zip(*coords)
+#     rows = np.array(rows)
+#     cols = np.array(cols)
+#     data = np.array(data)
+#     mask = coo_matrix((data, (rows, cols))).todense()
+#     return mask
 
-    Parameters
-    ----------
-    states : np.array
-       List of alignment states
-
-    Returns
-    -------
-    mask : np.array
-       Masked array.
-    """
-    i, j = 0, 0
-    res = []
-    mask = np.zeros((N, M))
-    for k in range(len(states)):
-        if states[k] == '1':
-            mask[i, j] = 0
-            i += 1
-        elif states[k] == '2':
-            mask[i, j] = 0
-            j += 1
-        elif states[k] == ':':
-            mask[i, j] = 1
-            i += 1
-            j += 1
-        elif states[k] == '.':
-            mask[i, j] = 0
-            i += 1
-            j += 1
-        else:
-            raise ValueError(f'{states[k]} is not recognized')
-    return mask
+def gap_mask(states : str, sparse=False):
+    st = np.array(list(map(tmstate_f, list(states))))
+    coords = states2edges(st)
+    data = np.ones(len(coords))
+    row, col = list(zip(*coords))
+    row, col = np.array(row), np.array(col)
+    N, M = max(row) + 1, max(col) + 1
+    idx = np.array(list(states)) == ':'
+    idx[0] = 1
+    data = data[idx]
+    row = row[idx]
+    col = col[idx]
+    mat = coo_matrix((data, (row, col)), shape=(N, M))
+    if sparse:
+        return mat
+    else:
+        return mat.toarray().astype(np.bool)
 
 
 def window(seq, n=2):

deepblast/losses.py
@@ -7,7 +7,7 @@ class AlignmentAccuracy:
 
 
 class MatrixCrossEntropy:
-    def __call__(self, Ytrue, Ypred, x_len, y_len):
+    def __call__(self, Ytrue, Ypred, x_len, y_len, G):
         """ Computes binary cross entropy on the matrix
 
         The matrix cross entropy loss is given by
@@ -27,20 +27,29 @@ class MatrixCrossEntropy:
         eps = 3e-8   # unfortunately, this is the smallest eps we can have :(
         Ypred = torch.clamp(Ypred, min=eps, max=1 - eps)
         for b in range(len(x_len)):
-            pos = torch.mean(
-                Ytrue[b, :x_len[b], :y_len[b]] * torch.log(
-                    Ypred[b, :x_len[b], :y_len[b]])
-            )
-            neg = torch.mean(
-                (1 - Ytrue[b, :x_len[b], :y_len[b]]) * torch.log(
-                    1 - Ypred[b, :x_len[b], :y_len[b]])
-            )
-            score += -(pos + neg)
+            pos = torch.masked_select(
+                    Ytrue[b, :x_len[b], :y_len[b]] * torch.log(
+                        Ypred[b, :x_len[b], :y_len[b]]),
+                    G[b, :x_len[b], :y_len[b]].bool()
+                )
+
+            neg = torch.masked_select(
+                    (1 - Ytrue[b, :x_len[b], :y_len[b]]) * torch.log(
+                        1 - Ypred[b, :x_len[b], :y_len[b]]),
+                    G[b, :x_len[b], :y_len[b]].bool()
+                )
+            # pos = Ytrue[b, :x_len[b], :y_len[b]] * torch.log(
+            #     Ypred[b, :x_len[b], :y_len[b]])
+            # neg = (1 - Ytrue[b, :x_len[b], :y_len[b]]) * torch.log(
+            #     1 - Ypred[b, :x_len[b], :y_len[b]])
+
+            score += -torch.mean(pos + neg)
+
         return score / len(x_len)
 
 
 class SoftPathLoss:
-    def __call__(self, Pdist, Ypred, x_len, y_len):
+    def __call__(self, Pdist, Ypred, x_len, y_len, G):
         """ Computes a soft path loss
 
         The soft path loss is given by
@@ -62,13 +71,16 @@ class SoftPathLoss:
         score = 0
         for b in range(len(x_len)):
             score += torch.norm(
-                Pdist[b, :x_len[b], :y_len[b]] * Ypred[b, :x_len[b], :y_len[b]]
+                torch.masked_select(
+                    Pdist[b, :x_len[b], :y_len[b]] * Ypred[b, :x_len[b], :y_len[b]],
+                    G[b, :x_len[b], :y_len[b]].bool()
+                )
             )
         return score / len(x_len)
 
 
 class SoftAlignmentLoss:
-    def __call__(self, Ytrue, Ypred, x_len, y_len):
+    def __call__(self, Ytrue, Ypred, x_len, y_len, G):
         """ Computes soft alignment loss as proposed in Mensch et al.
 
         The soft alignment loss is given by
@@ -98,6 +110,9 @@ class SoftAlignmentLoss:
         score = 0
         for b in range(len(x_len)):
             score += torch.norm(
-                Ytrue[b, :x_len[b], :y_len[b]] - Ypred[b, :x_len[b], :y_len[b]]
+                torch.masked_select(
+                    Ytrue[b, :x_len[b], :y_len[b]] - Ypred[b, :x_len[b], :y_len[b]],
+                    G[b, :x_len[b], :y_len[b]].bool()
+                )
             )
         return score / len(x_len)