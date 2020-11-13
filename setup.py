2bad760f3431b5805d080f4a0fff41f18e0a0f5f
konstin <konstin@mailbox.org>
2020-11-13 15:27:06 +0100

    Add biopython to requirements

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ setup(name='deepblast',
           'pytorch-lightning>=0.8.1',
           'matplotlib',
           'pillow',
+          'biopython>=1.78,<2.0'
       ],
       scripts=glob('scripts/*'),
       classifiers=classifiers,
